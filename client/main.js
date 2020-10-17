9c3304aaa9a422654e91a75ea0a55286bf350df9
Vishal Devireddy <TwoTau@users.noreply.github.com>
2020-10-17 15:28:18 -0700

    Add basic socket

--- client/main.js
+++ client/main.js
@@ -1,1 +1,17 @@
-const socket = io();
+const SOCKET_URL = 'http://localhost:8000';
+const socket = io(SOCKET_URL, { autoConnect: true });
+
+const usersListEl = document.querySelector('.users-list ul');
+
+socket.on('connect', () => {
+	console.log('Connected to the server via socket');
+});
+
+socket.on('disconnect', (reason) => {
+	console.log(`Connection to the server has been lost: ${reason}.`);
+});
+
+socket.on('users list', (data) => {
+	console.log(data);
+	usersListEl.innerHTML = data.map((u, i) => `<li>(${i+1}) ${u}</li>`).join('');
+});
--- index.js
+++ index.js
@@ -4,15 +4,40 @@ const cors = require('cors');
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
+const CLIENT_URL = 'http://localhost:3000';
 const PORT = process.env.PORT || 8000;
 
 app.use(cors({
-	origin: 'http://localhost:3000',
+	origin: CLIENT_URL,
 }));
 
 app.use(express.json());
 
 app.use(express.static(__dirname + '/client'));
+
+// list of connected members
+const connectedSockets = [];
+
+io.on('connection', (socket) => {
+	connectedSockets.push(socket);
+	console.log(`New client ${socket.id} connected. Users: ${connectedSockets.length}`);
+	sendUsersUpdate();
+
+	socket.on('disconnect', () => {
+		// remove from connectedSockets list
+		const index = connectedSockets.indexOf(socket);
+		if (index > -1) {
+			connectedSockets.splice(index, 1);
+		}
+
+		console.log(`Client ${socket.id} disconnected. Users: ${connectedSockets.length}`);
+		sendUsersUpdate();
+	});
+});
+
+function sendUsersUpdate() {
+	io.emit('users list', connectedSockets.map((s) => s.id));
+}
 
 http.listen(PORT, () => {
 	console.log(`Server listening on http://localhost:${PORT}`);
