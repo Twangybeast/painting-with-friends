521c228b4c2a071e9be16923c6b49aa505946eb6
Twangybeast <twangybeast@gmail.com>
2020-10-17 15:46:03 -0700

    Added an image for use with data

---
+++ image_manager.js
@@ -0,0 +1,12 @@
+const IMAGE_DATA = [
+    {
+        'id': 0,
+        'path': 'image1.jpg',
+        'colors': [
+            ['#7d8286'],
+            ['#cd8202'],
+            ['#f9ab97'],
+            ['#ffffff', '#000000']
+        ]
+    }
+];
