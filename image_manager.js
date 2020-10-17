346fd932147aff7a92bcaf713366da2330212113
Twangybeast <twangybeast@gmail.com>
2020-10-17 16:44:52 -0700

    Add two more imagesS

--- image_manager.js
+++ image_manager.js
@@ -8,6 +8,26 @@ const IMAGE_DATA = [
             ['#f9ab97'],
             ['#ffffff', '#000000']
         ]
+    },
+    {
+        'id': 1,
+        'path': 'image2.jpg',
+        'colors': [
+            ['#ffd33e'],
+            ['#ef5533'],
+            ['#211f20'],
+            ['#ffffff']
+        ]
+    },
+    {
+        'id': 2,
+        'path': 'image3.png',
+        'colors': [
+            ['#4d6b54'],
+            ['#964828'],
+            ['#fadc91'],
+            ['#966213']
+        ]
     }
 ];
 
