cce775b84072934ea655c7d6ee82827f0672caee
Johnson Kuang <johnsonkuang@outlook.com>
2020-10-18 00:43:20 -0700

    completed landing page

--- client/index.js
+++ client/index.js
@@ -10,5 +10,33 @@ nameChange.addEventListener('input', inputHandler);
 nameChange.addEventListener('propertychange', inputHandler);
 
 
+
 //propagates the drop-down
-const dropDown = 
+const fetch_rooms = async(dropDown) => {
+    fetch("http://localhost:8000/open-rooms")
+        .then(response => response.json())
+        .then(data => {
+            const rooms = data.rooms;
+            rooms.forEach(element => {
+                var el = document.createElement("li");
+                var div = document.createElement("div");
+                div.innerHTML = "Room " + element.room + ": " + element.current + " of " + element.max + " Players";
+                if(!element.started){
+                    var buttonEl = document.createElement("a");
+                    buttonEl.href = "http://localhost:8000/room.html?room="+element.room+"?name="+sessionStorage.getItem("name");
+                    var buttonTextEl = document.createElement("span");
+                    //TODO update class
+                    //buttonTextEl.className = "picon-p-add-news"; <-- add styling class here
+                    buttonTextEl.innerText = "Join Room!";
+                    buttonEl.appendChild(buttonTextEl);
+                    div.appendChild(buttonEl);
+                }
+                el.appendChild(div);                                
+                dropDown.appendChild(el);
+            });
+        });
+    
+};
+const dropDown = document.getElementById("rooms");
+fetch_rooms(dropDown);
+
