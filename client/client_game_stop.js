1740d9ded2b5c8fb77988dd1a0ad6e251c0956e5
Andrew Liu <andrewlliu@gmail.com>
2020-10-17 21:42:50 -0700

    confetti

--- client/client_game_stop.js
+++ client/client_game_stop.js
@@ -1,3 +1,29 @@
+function do_confetti() {
+  var myCanvas = document.createElement('canvas');
+  myCanvas.style.position = 'absolute'
+  myCanvas.style.top = 0
+  myCanvas.style.width = '100%'
+  document.body.appendChild(myCanvas);
+
+  var myConfetti = confetti.create(myCanvas, {
+    resize: true,
+    useWorker: true
+  });
+  myConfetti({
+    particleCount: 500,
+    startVelocity: 80,
+    spread: 180,
+    origin: {x: 0.5, y: 0.25}
+    // any other options from the global
+    // confetti function
+  });
+
+  setTimeout(() => {
+    myConfetti.reset();
+    document.body.removeChild(myCanvas)
+  }, 5000);
+}
+
 const game_stop = function(data) {
   const imageTitle = document.querySelector('.model-image > h2');
   const clock = document.querySelector('.timer');
@@ -6,4 +32,7 @@ const game_stop = function(data) {
   clock.textContent = "OUT OF TIME";
   clearInterval(intervalID);
   readyButton.classList.remove('hide');
+
+  do_confetti()
 }
+
