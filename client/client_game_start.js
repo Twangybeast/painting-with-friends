d0dab3f461b9e959fec2323e5b4cbca35ae8870a
Vishal Devireddy <TwoTau@users.noreply.github.com>
2020-10-18 00:46:12 -0700

    Add support for multiple colors

--- client/client_game_start.js
+++ client/client_game_start.js
@@ -2,7 +2,19 @@ function setBackgroundColor(color) {
   document.documentElement.style.setProperty('--bg', color);
 }
 
-const game_start = function (data, config) {
+function listenForNewColorSelection(socket, config) {
+  let els = document.querySelectorAll('.color-options .color-option');
+  for (let i = 0; i < els.length; i++) {
+    els[i].addEventListener('click', () => {
+      config.color = config.colors[i];
+      socket.emit('color_update', {
+        color: config.color,
+      });
+    });
+  }
+}
+
+const game_start = function (socket, data, config) {
   // model image
   const image = document.querySelector('img.model');
   // image name
@@ -14,16 +26,23 @@ const game_start = function (data, config) {
 
   let me = data.me;
 
-  let color = data.players[me].color;
+  let colors = data.players[me].colors;
+  config.colors = colors;
+  config.color = colors[0];
+  setBackgroundColor(config.color);
 
-  setBackgroundColor(color);
+  document.querySelector('.color-options').innerHTML = config.colors.map((c) => `<div class="color-option" style="background:${c}"></div>`).join('');
+  document.querySelector('.color-options .color-option:first-child').classList.add('selected');
+  listenForNewColorSelection(socket, config);
+  socket.emit('color_update', {
+    color: config.color,
+  });
 
   let path = data.image_path;
   let stop_time = data.stop_time; //UNIX time of stop time
   let deadline = new Date(stop_time);
   new Timer().initializeClock('timer', deadline);
-  config.color = color;
   image.src = path;
   imageTitle.textContent = data.image_name;
-  return data.players;
+  // return data.players;
 }
