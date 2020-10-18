35fd61604042b28079155c2ae936efeb6a8b13d6
Armand Mousavi <amousavi@cs.washington.edu>
2020-10-17 19:12:45 -0700

    Update client_game_start.js
    reduce time to what was set by stop_time of round

--- client/client_game_start.js
+++ client/client_game_start.js
@@ -5,7 +5,7 @@ const game_start = function (data) {
   var color = data.color;
   var path = data.image_path;
   var stop_time = data.stop_time; //UNIX time of stop time
-  var deadline = new Date(Date.parse(new Date()) + 60 * 60 * 1000);
+  var deadline = new Date(Date.parse(new Date()) + stop_time);
   new Timer().initializeClock('timer', deadline);
   ctx.strokeStyle = color;
   image.src = path;
